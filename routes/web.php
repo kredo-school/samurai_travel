586e5b7bd763f332c82e8399f07cf10412c364b9
ykhoop <yk-hoooooop@live.jp>
2023-05-02 20:09:30 +0900

    Added the routes and the layout view for administrator.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::group(['prefix' => 'admin', 'as' => 'admin.'], function()
+    {
+
+    });
+});
