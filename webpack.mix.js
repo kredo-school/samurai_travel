06ac94bc39eba40304c73bee9019cfe8d793bf0d
riyo-miyamoto <riyo.miyamoto3@gmail.com>
2023-05-05 20:47:40 +0900

    added ".mix.version()" in webpack.mix.js file

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,5 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/style.scss', 'public/css')
     .sourceMaps();
+
+mix.version();
