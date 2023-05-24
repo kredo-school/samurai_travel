567456aef4f5f6d7bbece2db86d1cc995a50dfc0
Tomoya Kihara <kihara0518@gmail.com>
2023-05-24 20:59:42 +0900

    fix google map api. but, Error if there is a waypoint.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/map.js') }}" defer></script>
     <script src="https://kit.fontawesome.com/f6dcff8a13.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
--- resources/views/users/top/plan.blade.php
+++ resources/views/users/top/plan.blade.php
@@ -3,83 +3,143 @@
         <h4 class="h4 text-capitalize">#PlanName</h4>
         <div class="row">
             <div class="col ">
-                <ul class="nav nav-pills mb-3" id="plan_id-tab" role="tablist">
+                <ul class="nav nav-pills mb-3" id="tab-1" role="tablist">
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="plan_id_day1-tab" data-bs-toggle="pill" data-bs-target="#plan_id_day1" type="button" role="tab" aria-controls="plan_id_day1" aria-selected="true">Day 1</button>
+                        <button class="nav-link active" id="plan-1-1-tab" data-bs-toggle="pill" data-bs-target="#plan-1-1" type="button" role="tab" aria-controls="plan-1-1" aria-selected="true">Day 1</button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="plan_id_day2-tab" data-bs-toggle="pill" data-bs-target="#plan_id_day2" type="button" role="tab" aria-controls="plan_id_day2" aria-selected="false">Day 2</button>
+                        <button class="nav-link" id="plan-1-2-tab" data-bs-toggle="pill" data-bs-target="#plan-1-2" type="button" role="tab" aria-controls="plan-1-2" aria-selected="false">Day 2</button>
                     </li>
                 </ul>
-                <div class="tab-content" id="plan_id-tabContent">
-                    <div class="tab-pane fade show active" id="plan_id_day1" role="tabpanel" aria-labelledby="plan_id_day1-tab" tabindex="0">
-                        <div class="float-start"><i class="fa-solid fa-arrow-down-long"></i></div>
-                        <div class="bg-plan p-2">
-                            <div class="p-2">
-                                <i class="fa-regular fa-cloc  k"></i> ### 9:00
-                                <i class="fa-regular fa-location-pin"></i> ### location name here
-                            </div>
-                            <div class="d-flex">
-                                <div class="px-2">
-                                    @if (file_exists(asset('/storage/xxx.jpg')))
-                                        <img src="{{ asset('/storage/xxx.jpg') }}" class="img-sm" alt="#">
-                                    @else
-                                        <img src="{{ asset('/images/no-image.png') }}" class="img-sm" alt="#">
-                                    @endif
+            </div>
+            <div class="row">
+                <div class="col tab-content" id="plan-1-tab-content">
+                    {{-- day 1 --}}
+                    <div class="tab-pane fade show active" id="plan-1-1" role="tabpanel" aria-labelledby="plan-1-1-tab" tabindex="0">
+                        {{-- place 1 --}}
+                        <div>
+                            <div class="float-start"><i class="fa-solid fa-arrow-down-long"></i></div>
+                            <div class="bg-plan p-2">
+                                <div class="p-2">
+                                    <i class="fa-regular fa-clock"></i> ### 9:00
+                                    <i class="fa-regular fa-location-pin"></i> ### location name here
                                 </div>
-                                <p class="place-desc px-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sint similique nemo expedita sequi quasi, possimus sit, ab voluptas, deleniti nihil nesciunt! A illum veniam obcaecati magni, quibusdam dicta accusantium.</p>
-                            </div>
-                        </div>
-                        <div class="">
-                            <i class="fa-solid fa-car"></i> 30 min
-                        </div>
-                        <div class="float-start"><i class="fa-solid fa-arrow-down-long"></i></div>
-                        <div class="bg-plan p-2">
-                            <div class="p-2">
-                                <i class="fa-regular fa-clock"></i> ### 9:00
-                                <i class="fa-regular fa-location-pin"></i> ### location name here
-                            </div>
-                            <div class="d-flex">
-                                <div class="px-2">
-                                    @if (file_exists(asset('/storage/xxx.jpg')))
-                                        <img src="{{ asset('/storage/xxx.jpg') }}" class="img-sm" alt="#">
-                                    @else
-                                        <img src="{{ asset('/images/no-image.png') }}" class="img-sm" alt="#">
-                                    @endif
+                                <div class="d-flex">
+                                    <div class="px-2">
+                                        @if (file_exists(asset('/storage/xxx.jpg')))
+                                            <img src="{{ asset('/storage/xxx.jpg') }}" class="img-sm" alt="#">
+                                        @else
+                                            <img src="{{ asset('/images/no-image.png') }}" class="img-sm" alt="#">
+                                        @endif
+                                    </div>
+                                    <p class="place-desc px-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sint similique nemo expedita sequi quasi, possimus sit, ab voluptas, deleniti nihil nesciunt! A illum veniam obcaecati magni, quibusdam dicta accusantium.</p>
                                 </div>
-                                <p class="place-desc px-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sint similique nemo expedita sequi quasi, possimus sit, ab voluptas, deleniti nihil nesciunt! A illum veniam obcaecati magni, quibusdam dicta accusantium.</p>
+                            </div>
+                            <div class="">
+                                <i class="fa-solid fa-car"></i> 30 min
                             </div>
                         </div>
-                        <div class="">
-                            <i class="fa-solid fa-car"></i> 30 min
+                        {{-- place 2 --}}
+                        <div>
+                            <div class="float-start"><i class="fa-solid fa-arrow-down-long"></i></div>
+                            <div class="bg-plan p-2">
+                                <div class="p-2">
+                                    <i class="fa-regular fa-clock"></i> ### 9:00
+                                    <i class="fa-regular fa-location-pin"></i> ### location name here
+                                </div>
+                                <div class="d-flex">
+                                    <div class="px-2">
+                                        @if (file_exists(asset('/storage/xxx.jpg')))
+                                            <img src="{{ asset('/storage/xxx.jpg') }}" class="img-sm" alt="#">
+                                        @else
+                                            <img src="{{ asset('/images/no-image.png') }}" class="img-sm" alt="#">
+                                        @endif
+                                    </div>
+                                    <p class="place-desc px-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sint similique nemo expedita sequi quasi, possimus sit, ab voluptas, deleniti nihil nesciunt! A illum veniam obcaecati magni, quibusdam dicta accusantium.</p>
+                                </div>
+                            </div>
+                            <div class="">
+                                <i class="fa-solid fa-car"></i> 30 min
+                            </div>
+                        </div>
+                        {{-- place 3 --}}
+                        <div>
+                            <div class="float-start"><i class="fa-solid fa-arrow-down-long"></i></div>
+                            <div class="bg-plan p-2">
+                                <div class="p-2">
+                                    <i class="fa-regular fa-clock"></i> ### 9:00
+                                    <i class="fa-regular fa-location-pin"></i> ### location name here
+                                </div>
+                                <div class="d-flex">
+                                    <div class="px-2">
+                                        @if (file_exists(asset('/storage/xxx.jpg')))
+                                            <img src="{{ asset('/storage/xxx.jpg') }}" class="img-sm" alt="#">
+                                        @else
+                                            <img src="{{ asset('/images/no-image.png') }}" class="img-sm" alt="#">
+                                        @endif
+                                    </div>
+                                    <p class="place-desc px-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sint similique nemo expedita sequi quasi, possimus sit, ab voluptas, deleniti nihil nesciunt! A illum veniam obcaecati magni, quibusdam dicta accusantium.</p>
+                                </div>
+                            </div>
+                            <div class="">
+                                <i class="fa-solid fa-car"></i> 30 min
+                            </div>
                         </div>
                     </div>
-                    <div class="tab-pane fade" id="plan_id_day2" role="tabpanel" aria-labelledby="plan_id_day2-tab" tabindex="0">
-                        <div class="float-start"><i class="fa-solid fa-arrow-down-long"></i></div>
-                        <div class="bg-plan p-2">
-                            <div class="p-2">
-                                <i class="fa-regular fa-clock"></i> ### 9:00
-                                <i class="fa-regular fa-location-pin"></i> ### location name here
-                            </div>
-                            <div class="d-flex">
-                                <div class="px-2">
-                                    @if (file_exists(asset('/storage/xxx.jpg')))
-                                        <img src="{{ asset('/storage/xxx.jpg') }}" class="img-sm" alt="#">
-                                    @else
-                                        <img src="{{ asset('/images/no-image.png') }}" class="img-sm" alt="#">
-                                    @endif
+                    {{-- day 2 --}}
+                    {{-- <div class="tab-pane fade" id="plan-{plans.id}-{plan_details.day}" role="tabpanel" aria-labelledby="plan-{plans.id}-{plan_details.id}-tab" tabindex="0"> --}}
+                    <div class="tab-pane fade" id="plan-1-2" role="tabpanel" aria-labelledby="plan-1-2-tab" tabindex="0">
+                        {{-- place 1 --}}
+                        <div>
+                            <div class="float-start"><i class="fa-solid fa-arrow-down-long"></i></div>
+                            <div class="bg-plan p-2">
+                                <div class="p-2">
+                                    <i class="fa-regular fa-clock"></i> ### 9:00
+                                    <i class="fa-regular fa-location-pin"></i> ### location name here
                                 </div>
-                                <p class="place-desc px-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sint similique nemo expedita sequi quasi, possimus sit, ab voluptas, deleniti nihil nesciunt! A illum veniam obcaecati magni, quibusdam dicta accusantium.</p>
+                                <div class="d-flex">
+                                    <div class="px-2">
+                                        @if (file_exists(asset('/storage/xxx.jpg')))
+                                            <img src="{{ asset('/storage/xxx.jpg') }}" class="img-sm" alt="#">
+                                        @else
+                                            <img src="{{ asset('/images/no-image.png') }}" class="img-sm" alt="#">
+                                        @endif
+                                    </div>
+                                    <p class="place-desc px-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sint similique nemo expedita sequi quasi, possimus sit, ab voluptas, deleniti nihil nesciunt! A illum veniam obcaecati magni, quibusdam dicta accusantium.</p>
+                                </div>
+                            </div>
+                            <div class="">
+                                <i class="fa-solid fa-car"></i> 30 min
                             </div>
                         </div>
-                        <div class="">
-                            <i class="fa-solid fa-car"></i> 30 min
+                        {{-- place 2 --}}
+                        <div>
+                            <div class="float-start"><i class="fa-solid fa-arrow-down-long"></i></div>
+                            <div class="bg-plan p-2">
+                                <div class="p-2">
+                                    <i class="fa-regular fa-clock"></i> ### 9:00
+                                    <i class="fa-regular fa-location-pin"></i> ### location name here
+                                </div>
+                                <div class="d-flex">
+                                    <div class="px-2">
+                                        @if (file_exists(asset('/storage/xxx.jpg')))
+                                            <img src="{{ asset('/storage/xxx.jpg') }}" class="img-sm" alt="#">
+                                        @else
+                                            <img src="{{ asset('/images/no-image.png') }}" class="img-sm" alt="#">
+                                        @endif
+                                    </div>
+                                    <p class="place-desc px-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sint similique nemo expedita sequi quasi, possimus sit, ab voluptas, deleniti nihil nesciunt! A illum veniam obcaecati magni, quibusdam dicta accusantium.</p>
+                                </div>
+                            </div>
+                            <div class="">
+                                <i class="fa-solid fa-car"></i> 30 min
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-                <p>### the map is here</p>
+                <div class="col" id="map"></div>
+                <script src="https://maps.googleapis.com/maps/api/js?key={{ config('api.google_map_api_key') }}&callback=initMap" defer>
+                </script>
             </div>
         </div>
     </div>
@@ -93,4 +153,7 @@
             </div>
         </div>
     </form>
+    <script>
+        const gm_plan = {!! json_encode($gm_plan) !!};
+    </script>
 </div>
