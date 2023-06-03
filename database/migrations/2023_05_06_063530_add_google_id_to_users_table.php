f93854edb890f4786f86babb7413ae3f40377601
ykhoop <yk-hoooooop@live.jp>
2023-06-03 14:12:18 +0900

    [Fix] Added the length of social authentication fields.

--- database/migrations/2023_05_06_063530_add_google_id_to_users_table.php
+++ database/migrations/2023_05_06_063530_add_google_id_to_users_table.php
@@ -14,7 +14,7 @@ class AddGoogleIdToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string("google_id")->nullable();
+            $table->string("google_id", 30)->nullable();
         });
     }
 
