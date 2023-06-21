a16dc9f2f4769c99441c679f26878a3365f50e09
ykhoop <yk-hoooooop@live.jp>
2023-06-21 19:46:41 +0900

    [Add] Created the migration files where image columns are nullable.

---
+++ database/migrations/2023_06_20_123227_change_image_column_nullable_in_places_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeImageColumnNullableInPlacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('places', function (Blueprint $table) {
+            $table->string('image',255)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('places', function (Blueprint $table) {
+            DB::statement('UPDATE `places` SET `image` = "" WHERE `image` IS NULL');
+            $table->string('image',255)->nullable(false)->change();
+        });
+    }
+}
---
+++ database/migrations/2023_06_20_123903_change_image_column_nullable_in_place_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeImageColumnNullableInPlaceImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('place_images', function (Blueprint $table) {
+            $table->string('image',255)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('place_images', function (Blueprint $table) {
+            DB::statement('UPDATE `place_images` SET `image` = "" WHERE `image` IS NULL');
+            $table->string('image',255)->nullable(false)->change();
+        });
+    }
+}
